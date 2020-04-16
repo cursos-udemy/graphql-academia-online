ca043e5282c2d34590acde78a6c7f3a5826d030d
gwfernandez <gwfernandez@gmail.com>
2020-04-16 19:20:05 -0300

    configuracion de apollo-server

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,27 @@
 import express from 'express';
 import compression from 'compression';
 import cors from 'cors';
+import { ApolloServer } from 'apollo-server-express';
 import { createServer } from 'http';
+import expressPlayGround from 'graphql-playground-middleware-express'
+import schema from './schema'
 
 const app = express();
 app.use('*', cors());
 app.use(compression());
 
-app.use('/', (req, res)=>{
-    res.send('Bienvenido a la Academia Online');
+const apolloServer = new ApolloServer({
+    schema,
+    introspection: true
 });
+apolloServer.applyMiddleware({ app });
+
+app.use('/', expressPlayGround({
+    endpoint: "/graphql"
+}));
 
 const port = 5001;
 const httpServer = createServer(app);
-httpServer.listen({port}, ()=> {
+httpServer.listen({ port }, () => {
     console.log(`Acamedia Online Started! ${port}`);
 });
