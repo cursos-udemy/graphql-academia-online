f488b0684633029e953dc810fccfd0ae4fea6cc6
gwfernandez <gwfernandez@gmail.com>
2020-04-17 12:31:15 -0300

    Mutation - agrego validacion de curso

--- src/resolvers/mutation.ts
+++ src/resolvers/mutation.ts
@@ -4,10 +4,18 @@ import { database } from "../data/data.store";
 const mutation: IResolvers = {
     Mutation: {
         addCourse(__: void, { course }): any {
+
+            const existCourse = database.courses.find(c => c.title === course.title);
+            if (existCourse) {
+                console.warn("Ya existe el curso " + course.title)
+                return existCourse;
+            } 
+
             const newCourse = { ...course }
             newCourse.id = String(database.courses.length + 1);
-            newCourse.reviews=[];
-            newCourse.students=[];
+            newCourse.reviews = [];
+            newCourse.students = [];    
+
             database.courses.push(newCourse);
             return newCourse;
         }
