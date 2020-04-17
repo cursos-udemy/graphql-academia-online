4b4a9878abf19d090c33b22b745e05d3fc4e78c5
gwfernandez <gwfernandez@gmail.com>
2020-04-17 14:10:17 -0300

    Mutation - actualizar un curso

--- src/resolvers/mutation.ts
+++ src/resolvers/mutation.ts
@@ -4,20 +4,28 @@ import { database } from "../data/data.store";
 const mutation: IResolvers = {
     Mutation: {
         addCourse(__: void, { course }): any {
-
             const existCourse = database.courses.find(c => c.title === course.title);
             if (existCourse) {
                 console.warn("Ya existe el curso " + course.title)
                 return existCourse;
-            } 
-
+            }
             const newCourse = { ...course }
             newCourse.id = String(database.courses.length + 1);
             newCourse.reviews = [];
-            newCourse.students = [];    
+            newCourse.students = [];
 
             database.courses.push(newCourse);
             return newCourse;
+        },
+        updateCourse(__: void, { course }): any {
+            const indexCourse = database.courses.findIndex(c => c.id === course.id);
+            if (indexCourse < 0) {
+                console.warn("No existe el curso " + course.id)
+                return null;
+            }
+            const { reviews } = database.courses[indexCourse];
+            database.courses[indexCourse] = { ...course, reviews }
+            return database.courses[indexCourse];
         }
     }
 };
