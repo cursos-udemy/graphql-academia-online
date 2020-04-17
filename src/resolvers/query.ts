c43ff0a35f2d076e271250ce1898a41e6143c49b
gwfernandez <gwfernandez@gmail.com>
2020-04-16 21:57:12 -0300

    Query - buscar estudiante x ID

--- src/resolvers/query.ts
+++ src/resolvers/query.ts
@@ -1,12 +1,15 @@
 import { IResolvers } from 'graphql-tools';
 import 'graphql-import-node'
 
-import {database} from './../data/data.store';
+import { database } from './../data/data.store';
 
 const query: IResolvers = {
     Query: {
-        students(): any  {
+        students(): any {
             return database.students;
+        },
+        student(__: void, { id }): any {
+            return database.students.find(student => student.id === id);
         }
     }
 }
